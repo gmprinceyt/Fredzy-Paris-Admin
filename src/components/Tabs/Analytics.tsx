41189019285b0fea52f4999d5252b26092b9c366
PRINCE KUAMR <gmprince420@gmail.com>
2025-08-24 09:48:05 +0530

    Fix Some bug for build

--- src/components/Tabs/Analytics.tsx
+++ src/components/Tabs/Analytics.tsx
@@ -1,5 +1,5 @@
 import { TrendingUp } from "lucide-react";
-import { Label, Pie, PieChart, Sector } from "recharts";
+import { Pie, PieChart, Sector } from "recharts";
 import type { PieSectorDataItem } from "recharts/types/polar/Pie";
 
 import {
